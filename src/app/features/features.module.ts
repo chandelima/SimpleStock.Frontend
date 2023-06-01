97a4a31002f2316f44683cf7aa061762c16e6745
Alexandre Lima <dev.alexandrelima@gmail.com>
2023-06-01 11:11:41 -0300

    feat: crud base

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+
+import { ConfirmationService, MessageService } from "primeng/api";
 import { OrdersComponent } from './orders/orders.component';
 import { CustomersComponent } from './customers/customers.component';
 import { ProductsComponent } from './products/products.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { PrimengModule } from "../template/primeng/primeng.module";
 
 @NgModule({
   declarations: [
-  
     OrdersComponent,
-       CustomersComponent,
-       ProductsComponent,
-       DashboardComponent
-  ]
+    CustomersComponent,
+    ProductsComponent,
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    PrimengModule
+  ],
+  providers: [ ConfirmationService, MessageService ]
 })
-export default class FeaturesModule { }
+export class FeaturesModule { }
--- src/app/features/products/products.component.ts
+++ src/app/features/products/products.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ProductService } from './services/product.service';
+import { ProductResponse } from './interfaces/product-response.interface';
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  products: ProductResponse[] = [];
+  searchText: string = "";
+
+  constructor(
+    private readonly productService: ProductService,
+    private readonly confirmationService: ConfirmationService,
+    private readonly messageService: MessageService
+  ) { }
 
   ngOnInit(): void {
+    this.getAllProducts();
   }
 
+  getAllProducts(): void {
+    this.productService.getProducts().subscribe(p => this.products = p);
+  }
+
+  confirmDeleteDialog(product: ProductResponse) {
+    this.confirmationService.confirm({
+      message: 'Tem certeza que deseja excluir o produto selecionado?',
+      header: 'Confirmação',
+      icon: 'ph ph-trash',
+      accept: () => {
+        this.messageService.add({ severity: 'info', summary: 'Confirmed', detail: 'You have accepted' });
+      },
+      reject: () => {
+        this.messageService.add({ severity: 'error', summary: 'Rejected', detail: 'You have rejected' });
+      }
+    })
+  };
 }
